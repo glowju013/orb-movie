afec47dd85b723629e212a5965bc4b786214e76d
glowju013 <nevertheless113@naver.com>
2025-11-16 19:02:46 +0900

    feat: main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="page">
+    <h1>Orb movie</h1>
+    <button class="primary-btn">리뷰 쓰기</button>
+    <button class="primary-btn">리뷰 보기</button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page {
+  background-color: #111;
+  color: #eee;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.primary-btn {
+   width: min(90%, 418px);
+  height: 100px;
+  background: #eee;
+  border: none;
+  font-size: 20px;
+  color: #000;
+  margin-top: 24px;
+  cursor: pointer;
+}
+</style>
